b12c7638d411d1cb8188648afb57c381faf87e79
ISSAM LAAFAR <80095490+issam22-laafer@users.noreply.github.com>
2024-07-04 15:44:58 +0000

    booking page

--- app/booking/page.jsx
+++ app/booking/page.jsx
@@ -11,7 +11,7 @@ export default function Page() {
   return (
     <div
       style={{
-        backgroundImage: 'url("/assets/home-bg.svg")',
+        backgroundImage: 'url("/assets/bg-2.svg")',
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         height: "100vh",
@@ -25,7 +25,7 @@ export default function Page() {
         <h1 className="login text-white md:pl-10 pl-0  mt-10 font-bold md:text-5xl text-4xl w-full md:text-start text-center">
           Welcome login!
         </h1>
-        <div className="md:mt-72 mt-20">
+        <div className="md:mt-60 mt-20">
           <h1 className=" text-white md:pl-10 pl-0  font-bold md:text-3xl text-2xl w-full md:text-start text-center">
             Available Bus :
           </h1>
@@ -49,24 +49,6 @@ export default function Page() {
               </div>
             </div>
             {/* ---------------- */}
-            <div className="unavailable-bus sm:w-72   w-72 m-10 h-32 rounded-3xl flex  flex-col justify-evenly items-evenly  bg-unavailable-gray bg-black">
-              <div className="container1 flex mt-2 justify-evenly items-center">
-                <div className="bus-time w-32 text-center p-2 text-xl rounded-3xl font-bold bg-lightgreen">
-                  9pm bus
-                </div>
-                <div className="time-remaining text-3xl text-white font-extrabold">
-                  00:42
-                </div>
-              </div>
-              <div className="conatiner2 flex  justify-evenly items-center">
-                <div className="available-places text-center w-32 text-white font-bold">
-                  12 places left
-                </div>
-                <button className="booking-button rounded-2xl bg-unavailable-blue  font-bold text-2xl w-24">
-                  Book
-                </button>
-              </div>
-            </div>
             <div className="unavailable-bus sm:w-72   w-72 m-10 h-32 rounded-3xl flex  flex-col justify-evenly items-evenly  bg-unavailable-gray bg-black">
               <div className="container1 flex mt-2 justify-evenly items-center">
                 <div className="bus-time w-32 text-center p-2 text-xl rounded-3xl font-bold bg-lightgreen">
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,7 +4,7 @@ const nav = () => {
   return (
     <div className="nav w-full fixed top-0 flex md:justify-evenly justify-between items-center bg-transparent p-3">
       <img src="/assets/notif.svg" alt="notif" className="notif" />
-      <img src="/assets/13-bus.svg" alt="logo" className="bus-13" />
+      <img src="/assets/13-bus2.svg" alt="logo" className="bus-13" />
       <img src="/assets/logout.svg" alt="logout" className="logout" />
     </div>
   );
